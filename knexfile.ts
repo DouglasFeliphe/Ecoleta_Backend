0b48e65d431d408e91b45388be98110c7634e25d
DouglasFeliphe <douglasfeliphe@hotmail.com>
2020-08-27 03:33:12 -0300

    alterações na config do knex com banco do heroku

--- knexfile.ts
+++ knexfile.ts
@@ -14,16 +14,17 @@ module.exports = {
         }
     },
     production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        // connection: {
+        //     database: 'my_db',  
+        //     user: 'username',
+        //     password: 'password'
+        // },
+        // pool: {
+        //     min: 2,
+        //     max: 10
+        // },
         migrations: {
             directory: path.resolve(__dirname, 'src', 'database', 'migrations')
         },
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,13 +2,9 @@ import knex from 'knex';
 import path from 'path';
 
 const connection = knex({
-    client: 'mysql',
+    client: 'sqlite3',
     connection: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'admin',
-        database: 'nlw-ecoleta'
-        // filename: path.resolve(__dirname, 'database.sqlite')
+        filename: path.resolve(__dirname, 'database.sqlite')
     },
     useNullAsDefault: true
 })
