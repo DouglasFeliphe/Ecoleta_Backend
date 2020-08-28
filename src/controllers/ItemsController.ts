133d16ae964b8d91fcc9c1ff6be1d5064d11b5c9
DouglasFeliphe <douglasfeliphe@hotmail.com>
2020-08-28 15:01:38 -0300

    alteração no link de listagem de image items

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,13 +10,12 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.42.52:3333/uploads/${item.image}`
+                image_url: process.env.PORT + `/uploads/${item.image}`
             }
         })
 
         return response.json(serializedItems)
     }
-
 }
 
 export default ItemsController
