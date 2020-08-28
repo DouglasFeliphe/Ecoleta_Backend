0b4b68081ebaa8726af7de72db562295bb91a8ce
DouglasFeliphe <douglasfeliphe@hotmail.com>
2020-08-28 15:20:29 -0300

    alteração no link de listagem de image items

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: process.env.APP_URL + `/uploads/${item.image}`
+                image_url: process.env.APP_URL + `uploads/${item.image}`
             }
         })
 
