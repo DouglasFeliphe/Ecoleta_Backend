d27593e12eaeae5d9526a54963afdbf24cdf77f2
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-04-09 00:25:02 -0300

    alterações no deploy do heroku

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import routes from './routes';
 import path from 'path';
 import cors from 'cors';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 const app = express()
 
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-dotenv.config()
+// dotenv.config()
 
 app.listen(process.env.PORT || 3333, function () {
     console.log(`Express server listening on port 3333 in ${app.settings.env} mode`)
