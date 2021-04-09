056e772b25e1d75046262416c326fbb821e295fa
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-04-09 00:40:56 -0300

    alterações no deploy do heroku

--- knexfile.ts
+++ knexfile.ts
@@ -13,11 +13,11 @@ module.exports = {
             directory: path.resolve(__dirname, 'src', 'database', 'seeds')
         }
     },
-    production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-        },
-    }
+    // production: {
+    //     client: 'pg',
+    //     connection: process.env.DATABASE_URL,
+    //     migrations: {
+    //         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    //     },
+    // }
 }
