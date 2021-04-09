787c6b02047c02f8a7ada77b5cbe4caae2f8d639
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-04-08 22:14:30 -0300

    alterações no deploy com heroku

--- src/routes.ts
+++ src/routes.ts
@@ -22,7 +22,7 @@ routes.get('/points', pointsController.index)
 // mostrar ponto de coleta espeçífico
 routes.get('/points/:id', pointsController.show)
 
-routes.post('/point', upload.single('image'), pointsController.create)
+routes.post('/point', pointsController.create)
 
 
 export default routes
