144e83fb84369fdf0fb533a951112c6afcb03126
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-04-09 10:52:00 -0300

    envio de imagens com Dropzone

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -53,7 +53,7 @@ class PointsController {
         const transaction = await connection.transaction()
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
