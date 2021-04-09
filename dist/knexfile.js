6902a194fb1ee0ed4915e29c90176974e0c24552
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-04-09 01:00:40 -0300

    alterações no deploy do heroku

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -17,6 +17,18 @@ module.exports = {
             directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
         }
     },
+    production: {
+        client: 'sqlite3',
+        connection: {
+            filename: path_1.default.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        },
+        migrations: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
+        }
+    }
     // production: {
     //     client: 'pg',
     //     connection: process.env.DATABASE_URL,
--- knexfile.ts
+++ knexfile.ts
@@ -13,6 +13,19 @@ module.exports = {
             directory: path.resolve(__dirname, 'src', 'database', 'seeds')
         }
     },
+    production: {
+        client: 'sqlite3',
+        connection: {
+            filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        },
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+        }
+    }
+
     // production: {
     //     client: 'pg',
     //     connection: process.env.DATABASE_URL,
