5bd4ed4b335533a8ccd48a9e459eb114babbb210
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-04-14 09:26:34 -0300

    alterações

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { celebrate, Joi } from 'celebrate'
+import celebrate from '../config/celebrate';
 
 import multer from 'multer';
 import multerConfig from '../config/multer';
@@ -25,21 +25,9 @@ routes.get('/points/:id', pointsController.show)
 
 routes.post('/point',
     upload.single('image'),
-    celebrate({
-        body: Joi.object().keys({
-            name: Joi.string().required(),
-            email: Joi.string().required().email(),
-            whatsapp: Joi.number().required(),
-            latitude: Joi.number().required(),
-            longitude: Joi.number().required(),
-            city: Joi.string().required(),
-            uf: Joi.string().required().max(2),
-            items: Joi.string().required()
-        })
-    }, {
-        abortEarly: false //validar todos os campos ao mesmo tempo
-    }),
-    pointsController.create)
+    celebrate(),
+    pointsController.create
+)
 
 
 export default routes
